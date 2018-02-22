9a4b7138abebfa9c4f34352ff63c2bcd2e418eb3
Christian Rojas <cr259@afsconnect2.njit.edu>
2018-02-21 21:54:10 -0500

    compare date and tar

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-	$date =  date('Y-m-d', time());
+	$date =  date('Y/m/d', time());
 	echo "The value of \$date: ".$date."<br>";
 	$tar = "2017/05/24";
 	echo "The value of \$tar: ".$tar."<br>";
+
+	if($date > $tar){
+		echo "The Future"."<br>";
+	}
+	elseif($date < $tar){
+		echo "The Past"."<br>"; 
+	}
+	else{
+		echo "Oops"."<br>"; 
+	}
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
