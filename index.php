db1f82f1d9f39de126e8e0907ad525cc76bddd6a
Christian Rojas <cr259@afsconnect2.njit.edu>
2018-02-22 11:11:26 -0500

    get ascII of first char in a array

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
 
 	echo str_word_count($date)."<br>";
 	echo strlen($date)."<br>";
+	echo ord($date)."<br>";
 
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
